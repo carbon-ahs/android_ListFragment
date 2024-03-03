4d24d4ddb72a733647f05523c7b6d054ad7c39d9
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-03 10:21:11 +0600

    vid 295 done, list of courses showing

--- app/src/main/java/com/axiagroups/listfragment/MyFragment.java
+++ app/src/main/java/com/axiagroups/listfragment/MyFragment.java
@@ -8,10 +8,28 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
-public class MyFragment extends Fragment {
+import com.axiagroups.listfragment.data.Course;
+import com.axiagroups.listfragment.data.CourseArrayAdaptor;
+import com.axiagroups.listfragment.data.CourseData;
+
+import java.util.List;
+
+public class MyFragment extends ListFragment {
+    List<Course> courses = new CourseData().courseList();
     public MyFragment(){
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CourseArrayAdaptor adaptor = new CourseArrayAdaptor(getActivity(),
+                R.layout.course_listitem, courses);
+
+        setListAdapter(adaptor);
     }
 
     @Nullable
